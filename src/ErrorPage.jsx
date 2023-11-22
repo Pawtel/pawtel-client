029dd6609e1f43b3ec1b5566d16b7a17854c95a2
Luke Wheldale <lukewheldale@gmail.com>
2023-11-22 13:06:04 +1030

    spelling fix

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const ErrorPage = () => {
 	return (
 		<div>
-			<h1>Uh Oh!, This route does not Exist!</h1>
+			<h1>Uh Oh, this route does not Exist!</h1>
 			<Link to="/">
 				You can go back to the home page by clicking here.
 			</Link>
