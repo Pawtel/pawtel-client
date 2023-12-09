ac3af27e73aa030bdd36a9d25f1cbe0f5f8583b9
Luke Wheldale <lukewheldale@gmail.com>
2023-12-09 15:12:17 +1030

    added home, styling for homepage title font

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import "../styles/home.css";
+import homeImage from "../assets/images/pawtel-home-main.avif";
 
 const HomePage = () => {
 	return (
@@ -12,7 +13,12 @@ const HomePage = () => {
 			<main>
 				<section>
 					<div className="home-container">
-						<h1>Home Page</h1>
+						<h1 className="home-title">Pawtel</h1>
+						<img
+							className="home-image"
+							src={homeImage}
+							alt="Image of Dog and Cat in field"
+						></img>
 					</div>
 				</section>
 				<section></section>
