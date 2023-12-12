0c2a2b87081ced4e637906659ca0a5342c03b3db
Luke Wheldale <lukewheldale@gmail.com>
2023-12-12 16:58:31 +1030

    increased font sizing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,11 @@ import logoImage from "../assets/images/Pawtel-logo.png";
 const Footer = () => {
 	return (
 		<div className="footer-container">
+			<div className="social-icons">
+				<div>1</div>
+				<div>2</div>
+				<div>3</div>
+			</div>
 			<img className="logo-image" src={logoImage} alt="Pawtel Logo"></img>
 		</div>
 	);
