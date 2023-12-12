1c00827b7b5ad7c5aed3766f0af18eb4f2105900
Luke Wheldale <lukewheldale@gmail.com>
2023-12-12 17:27:45 +1030

    feat: footer and homepage finished. Social media links complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,28 @@
 import "../styles/footer.css";
 import logoImage from "../assets/images/Pawtel-logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
 	return (
 		<div className="footer-container">
 			<div className="social-icons">
-				<div>1</div>
-				<div>2</div>
-				<div>3</div>
+				<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+					<FontAwesomeIcon
+						className="footer-icons"
+						icon={faFacebook}
+					/>
+				</a>
+				<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+					<FontAwesomeIcon
+						className="footer-icons"
+						icon={faInstagram}
+					/>
+				</a>
+				<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+					<FontAwesomeIcon className="footer-icons" icon={faPhone} />
+				</a>
 			</div>
 			<img className="logo-image" src={logoImage} alt="Pawtel Logo"></img>
 		</div>
