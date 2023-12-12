19b70bfc458fe6914544660f784e95f04db704ee
Luke Wheldale <lukewheldale@gmail.com>
2023-12-12 13:47:28 +1030

    added css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -21,7 +21,49 @@ const HomePage = () => {
 						></img>
 					</div>
 				</section>
-				<section></section>
+				<h3 className="testimonial-title">Testimonials</h3>
+				<section className="main-container">
+					<div>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing
+							elit. Maxime laboriosam repellendus, itaque, iusto
+							atque aliquam doloribus laborum amet quis delectus
+							unde possimus veniam perferendis dolor quam odio
+							tenetur voluptatibus dolore! Lorem ipsum dolor sit
+							amet consectetur adipisicing elit. Ducimus saepe aut
+							maiores tempore explicabo pariatur inventore itaque,
+							illum doloremque voluptatem aliquam in ipsam cum
+							earum natus ullam quisquam quia magni.
+						</p>
+					</div>
+					<div>
+						<p>
+							Lorem, ipsum dolor sit amet consectetur adipisicing
+							elit. Voluptas tempore ipsum at minima dicta
+							deleniti alias veritatis cum laudantium repellat.
+							Ad, aliquid ducimus. Quaerat delectus vitae illum
+							consequuntur totam distinctio. Lorem ipsum dolor sit
+							amet consectetur, adipisicing elit. Expedita
+							voluptates voluptate at dolorem error? Cum quis quos
+							praesentium iusto voluptatibus, repellat, numquam ab
+							nesciunt, ratione fugit voluptate ea. Quaerat, ut?
+						</p>
+					</div>
+					<div>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Adipisci nemo enim voluptates rem iste
+							possimus dignissimos, voluptatibus eveniet natus
+							expedita, consectetur illum delectus. Rerum
+							similique reprehenderit facere voluptate magni nam.
+							Lorem ipsum dolor sit amet consectetur, adipisicing
+							elit. Quo veniam maxime, quibusdam error itaque
+							magnam sit obcaecati animi enim necessitatibus atque
+							odio nesciunt corporis modi placeat tempore nobis
+							iste voluptatem.
+						</p>
+					</div>
+				</section>
 			</main>
 
 			<footer>
