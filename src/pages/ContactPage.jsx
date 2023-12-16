984a1dd1091aa9e9027fa7fa30719f6c3297fd68
Luke Wheldale <lukewheldale@gmail.com>
2023-12-16 14:36:03 +1030

    contact form complete - seciton for adding handleSubmit to back end included

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,5 +1,7 @@
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import ContactForm from "../components/ContactForm";
+import "../styles/contact.css";
 
 const ContactPage = () => {
 	return (
@@ -9,7 +11,12 @@ const ContactPage = () => {
 			</header>
 
 			<main>
-				<h1 className="title">Contact Page</h1>
+				<h1 className="title">Contact Us</h1>
+				<p className="contact-info-message">
+					Send us a message and one of our friendly staff will get
+					back to you.
+				</p>
+				<ContactForm />
 			</main>
 
 			<footer>
