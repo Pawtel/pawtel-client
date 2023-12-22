5a1a3da12759c770bd1b32207c0d4554c9fd3a41
Luke Wheldale <lukewheldale@gmail.com>
2023-12-22 18:11:29 +1030

    added comments for some tests

--- src/components/NavBar.test.jsx
+++ src/components/NavBar.test.jsx
@@ -4,18 +4,21 @@ import NavBar from "./NavBar";
 import { expect, test } from "vitest";
 
 test("NavBar renders correctly", async () => {
+	// Render the NavBar component within a MemoryRouter to provide context for the Link components
 	const { getByText, unmount } = render(
 		<MemoryRouter>
 			<NavBar />
 		</MemoryRouter>
 	);
 
+	// Check if the Links are in the document
 	expect(getByText("Home")).toBeInTheDocument();
 	expect(getByText("About")).toBeInTheDocument();
 	expect(getByText("Contact")).toBeInTheDocument();
 	expect(getByText("Login")).toBeInTheDocument();
 	expect(getByText("Book Now")).toBeInTheDocument();
 
+	// Check if the Links have the correct href attribute
 	const bookNowLink = getByText("Book Now");
 	expect(bookNowLink).toHaveAttribute("href", "/book");
 
@@ -31,5 +34,6 @@ test("NavBar renders correctly", async () => {
 	const loginLink = getByText("Login");
 	expect(loginLink).toHaveAttribute("href", "/login");
 
+	// Unmount the component after the test
 	unmount();
 });
