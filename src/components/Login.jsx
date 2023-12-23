8a36a5fac9488b65154d8455b0aef8b1eea008bb
Luke Wheldale <lukewheldale@gmail.com>
2023-12-23 16:41:49 +1030

    rename css file

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import "../styles/Login.css";
+import "../styles/login.css";
 
 const Login = () => {
 	const [email, setEmail] = useState("");
